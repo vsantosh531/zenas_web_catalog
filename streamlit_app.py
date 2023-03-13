cf3c40db824cc407f338ec5df0ea2565dc0c5165
vsantosh531 <126091006+vsantosh531@users.noreply.github.com>
2023-03-13 19:43:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
-
-streamlit.title('My test title')
 import snowflake.connector
+streamlit.title('My test title')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
